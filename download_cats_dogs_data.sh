2edfc5c2bbe9eaff0012eebcbcca695c5f54027d
mpriessner <43908762+mpriessner@users.noreply.github.com>
2020-06-06 21:10:05 +0100

    Add files via upload

---
+++ download_cats_dogs_data.sh
@@ -0,0 +1,4 @@
+fileid="1vzBevBcqQh789T7x20A2avELxAc3ek55"
+filename="original.zip"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
